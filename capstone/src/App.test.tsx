252961620a23d1f61f401b075ab699ba56a932fa
Jacklyn McAtee <jrmcatee1926@students.lcstate.edu>
2025-02-11 21:30:51 -0800

    added unit tests for create recipe and recipe modifier page

--- capstone/src/App.test.tsx
+++ capstone/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import IngredientPage from './components/IngredientList';
-import {MemoryRouter} from 'react-router-dom';
-import populateList from './components/IngredientList';
+import CreateRecipes from './components/CreateRecipes';
+import RecipeModifier from './components/RecipeModifier';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders without crashing', () => {
   const { baseElement } = render(<App />);
@@ -17,5 +18,29 @@ test('renders ingredient page', () => {
     </MemoryRouter>
   );
   expect(screen.getByText('Ingredients')).toBeInTheDocument();
+});
 
+// unit testing for the Create Recipes page to isolate the component and test if it is working as expected
+test('renders Create Recipes page', () => {
+  render(
+    <MemoryRouter initialEntries={['/CreateRecipes']}>
+      <CreateRecipes />
+    </MemoryRouter>
+  );
+  expect(screen.getByPlaceholderText('Enter Recipe Name')).toBeInTheDocument();
+  expect(screen.getByPlaceholderText('Enter Ingredient Name')).toBeInTheDocument();
+  expect(screen.getByPlaceholderText('Enter Ingredient Amount')).toBeInTheDocument();
+  expect(screen.getByPlaceholderText('Enter Instructions')).toBeInTheDocument();
+});
+
+// unit testing for the Recipe Modifier page to isolate the component and test if it is working as expected
+test('renders Recipe Modifier page', () => {
+  render(
+    <MemoryRouter initialEntries={['/RecipeModifier']}>
+      <RecipeModifier />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('Recipe Modifier')).toBeInTheDocument();
+  // expect search bar to be present
+  expect(screen.getByPlaceholderText('Search for recipes')).toBeInTheDocument();
 });
