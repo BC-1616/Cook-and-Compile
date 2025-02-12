93ddcdd33a671908162752df1be5e4c46052ad31
ajburrell <ajburrell2029@students.lcstate.edu>
2025-02-11 18:16:44 -0800

    add quick test for rendering recipe page

--- capstone/src/App.test.tsx
+++ capstone/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import IngredientPage from './pages/IngredientList';
+import Recipe from './pages/Recipe';
 import {MemoryRouter} from 'react-router-dom';
 import populateList from './pages/IngredientList';
 
@@ -18,4 +19,9 @@ test('renders ingredient page', () => {
   );
   expect(screen.getByText('Ingredients')).toBeInTheDocument();
 
+});
+
+test('renders recipe page', () => {
+  const { baseElement } = render(<Recipe />);
+  expect(baseElement).toBeDefined();
 });
