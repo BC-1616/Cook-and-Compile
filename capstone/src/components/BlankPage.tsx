d3e05f47e1480c8f124129fb198e4080e7acb7e8
Clay Shumaker <cshumaker2502@students.lcstate.edu>
2025-03-19 10:50:58 -0700

    Getting somewhere with exporting recipes.

--- capstone/src/components/BlankPage.tsx
+++ capstone/src/components/BlankPage.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonButton } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
-import { signOut } from 'firebase/auth';  // Import signOut from Firebase
+import { signOut, onAuthStateChanged } from 'firebase/auth';  // Import signOut from Firebase
 import { auth } from '../firebase_setup/firebase';  // Import your Firebase auth instance
 import { useUser } from './UserContext';  // Assuming you are using context for global state management
 import handleAuth from '../handles/handleAuth'; // Import handleAuth hook
+import { handleFetchRecipes } from '../handles/handleFetchRecipes';
 import '../Styles/BlankPage.css';
 
+interface Recipe {
+    id: string;
+    image: string;
+    name: string;
+    ingredients: { [key: string]: string };
+    instructions: string;
+    tags: string; // Maybe change to a list if tags are multiple
+    userAllergic: boolean,
+    userPref: boolean
+}
+
 const BlankPage: React.FC = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const getRecipes = async () => { 
+      try{
+        const userRecipes = await handleFetchRecipes();
+        setRecipes(userRecipes);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getRecipes();
+  }, []);
+  console.log("recipes:");
+  console.log(recipes);
+  
   const { user } = handleAuth();  // Use the handleAuth hook to get user data
   const history = useHistory();
-
+  
   // Handle sign out
   const handleSignOut = async () => {
     try {
@@ -28,14 +57,33 @@ const BlankPage: React.FC = () => {
       console.log("No user data available to export");
       return;
     }
-
-    const userData = {
+   
+    // This will be the JSON object
+    const userPersonalData = {
       uid: user.uid,
       displayName: user.displayName,
       email: user.email,
       photoURL: user.photoURL,
     };
+    const otherData = {
+      type: "recipes",
+      recipes: recipes,
+    }
 
+    const userData = {
+      ppd: userPersonalData,
+      other: otherData,
+    };
+    
+    /*
+    const jsonRecipeData = JSON.stringify(userRecipes, null, 2);
+    const recipeBlob = new Blob([jsonRecipeData], { type: 'application/json' });
+    const recipeURL = URL.createObjectURL(recipeBlob);
+    const rlink = document.createElement('a');  // Create an anchor element
+    rlink.href = recipeURL;  // Set the download link
+    rlink.download = 'userData.json';  // Specify the filename for the downloaded JSON file
+    rlink.click();  // Simulate a click to trigger the download
+    */
     const jsonData = JSON.stringify(userData, null, 2);  // Convert user data to JSON string
     const blob = new Blob([jsonData], { type: 'application/json' });  // Create a Blob with JSON data
     const url = URL.createObjectURL(blob);  // Create an object URL for the Blob
