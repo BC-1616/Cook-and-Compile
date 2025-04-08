fee44c6da62d516e7f3e2ac61db04626044f227e
ajburrell <ajburrell2029@students.lcstate.edu>
2025-04-08 14:39:32 -0700

    Set up the grid of recipes. Got search working.

--- capstone/src/components/Recipe_outdated.tsx
+++ capstone/src/components/Recipe_outdated.tsx
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonItem, IonButton , IonInput, IonModal, IonText, IonTextarea, IonIcon, IonSearchbar} from '@ionic/react';
-import { removeCircleOutline, addCircleOutline } from 'ionicons/icons';
-import '../Styles/RecipeModifier.css';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonItem, IonButton , IonInput} from '@ionic/react';
 import { handleFetchRecipes } from '../handles/handleFetchRecipes';
-import { handleCreateRecipe } from '../handles/handleCreateRecipe';
-import { handleDeleteRecipe } from '../handles/handleDeleteRecipe';
-import { handleEditRecipe } from '../handles/handleEditRecipe';
 import { handleFetchAllergy, checkIfAllergic, includesStringInArray } from '../handles/handleAllergy';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase_setup/firebase';  // Ensure you have the auth instance setup
 import { saveURL } from '../handles/handleImages';
-import { set } from 'cypress/types/lodash';
 import '../Styles/Recipe.css';
 
 const Recipe: React.FC = () => {
   const [recipes, setRecipes] = useState<any[]>([]);
   const [allergies, setAllergies] = useState<any>([]);
-  const [filteredRecipes, setFilteredRecipes] = useState<any[]>([]);
-  const [searchText, setSearchText] = useState<string>('');
   const [preferences, setPref] = useState<any>([])
   const [currentRecipe, setCurrentRecipe] = useState<any | null>(null);
   const [loading, setLoading] = useState(true);
@@ -46,7 +38,6 @@ const Recipe: React.FC = () => {
           allergyData ? setAllergies(allergyData[0].allergies) : setAllergies([""]);
           allergyData ? setPref(allergyData[1].pref_list) : setPref([""]);
           setRecipes(updatedRecipes);
-          setFilteredRecipes(updatedRecipes);
         } catch (error) {
           console.error("Error fetching recipes:", error);
         } finally {
@@ -65,19 +56,6 @@ const Recipe: React.FC = () => {
   // Updates page to the selected recipe
   const click = (recipe: any) => {
     setCurrentRecipe(recipe);
-  };
-
-  const handleSearch = (recipes: any[], searchTerm: string): any[] => {
-    const lowerCaseSearchTerm = searchTerm.toLowerCase();
-    return recipes.filter((recipe) =>
-      recipe.name.toLowerCase().includes(lowerCaseSearchTerm)
-    );
-  };
-
-  const handleSearchInput = (event: CustomEvent) => {
-    const query = event.detail.value;
-    setSearchText(query);
-    setFilteredRecipes(handleSearch(recipes, query));
   };
 
   if (loading) {
@@ -104,52 +82,39 @@ const Recipe: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          {/* Removed menu button as it is not needed with new nav bar and added CSS to move page tile below Navbar for web */}
           <IonTitle id="title">Recipes</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        {/* Search Bar */}
-        <IonSearchbar
-          value={searchText}
-          onIonInput={handleSearchInput}
-          placeholder="Search for recipes"
-        />
-
         {currentRecipe ? (
-          /*Button is for when the recipe instructions are displayed so they can return to button page*/
+          /*Button is for when the recipe intructions are displayed so they can return to button page*/
           /*Added the recipe title to the information once button is clicked*/
           <div>
-            <IonButton onClick={() => setCurrentRecipe(null)}>Back</IonButton>
+            <IonButton onClick={() => setCurrentRecipe(null)}>Back</IonButton>           
             {currentRecipe.userAllergic === true ? (
-              <p style={{ color: 'red' }}>
-                You are <strong>ALLERGIC</strong> to this recipe
-              </p>
+              <p style={{color: 'red'}}>You are <strong>ALLERGIC</strong> to this recipe</p>
             ) : (
               <p></p>
             )}
             <IonItem>
-              <h2>
-                <strong>{currentRecipe.name}</strong>
-              </h2>
+              <h2><strong>{currentRecipe.name}</strong></h2>
             </IonItem>
             <div id="basic_recipe_info">
               <h3>Ingredients:</h3>
               <ul>
-                {Object.entries(currentRecipe.ingredients).map(([ingredientName, amount], index) =>
-                  includesStringInArray(allergies, ingredientName) ? ( // The Allergic ingredient takes precedent
-                    <li key={index} style={{ color: 'red' }}>
-                      {ingredientName}: {amount as string}
-                    </li>
-                  ) : includesStringInArray(preferences, ingredientName) ? (
-                    <li key={index} style={{ color: 'green' }}>
-                      {ingredientName}: {amount as string}
-                    </li>
+              {Object.entries(currentRecipe.ingredients).map(([ingredientName, amount], index) => (
+                includesStringInArray(allergies, ingredientName) ? ( // The Allergic ingredient takes precedent
+                  <li key={index} style={{color: 'red'}}>{ingredientName}: {amount as string}</li>
+                ) : (
+                  includesStringInArray(preferences, ingredientName) ? (
+                    <li key={index} style={{color: 'green'}}>{ingredientName}: {amount as string}</li>
                   ) : (
-                    <li key={index}>
-                      {ingredientName}: {amount as string}
-                    </li>
+                    <li key={index}>{ingredientName}: {amount as string}</li>
                   )
-                )}
+                )
+              ))}
+              
               </ul>
               <h3>Instructions:</h3>
               <p>{currentRecipe.instructions}</p>
@@ -158,6 +123,9 @@ const Recipe: React.FC = () => {
 
               {/* Image URL Submission */}
               <h3>Change Recipe Image:</h3>
+              {/* Input for image URL will accept anything causing image to be blank if bad url is submitted
+              will need to add some constraints later but for now,
+              if the user just hits sumbit it reuploadeds the url so no changes will be made for blank text */}
               <IonInput
                 placeholder="Enter image URL"
                 value={imageURLs[currentRecipe.id] || ''}
@@ -166,35 +134,30 @@ const Recipe: React.FC = () => {
               <IonButton onClick={() => urlSubmit(currentRecipe.id)}>Submit</IonButton>
             </div>
           </div>
-        ) : filteredRecipes.length === 0 ? (
+      ) : (
+        recipes.length === 0 ? (
           <div>No recipes found</div>
         ) : (
-          <div className="recipe_grid">
-            {filteredRecipes.map((recipe, index) => (
-              <div key={recipe.id} id={index === filteredRecipes.length - 1 ? 'last-recipe' : ''}>
+          recipes.map((recipe, index) => (
+            <div key={recipe.id} id={index === recipes.length - 1 ? "last-recipe" : ""}>
                 <IonButton
-                  className={
-                    recipe.userAllergic === true ? 'recipe_button_allergic' : 'recipe_button'
-                  }
+                  //uses the css description for the button size and I think the round looks better but it can easily be changed
+                  //the height and width can easily be changed, will have to come back to see what looks nicest
+                  className={recipe.userAllergic === true ? "recipe_button_allergic" : "recipe_button"}
                   shape="round"
-                  fill="clear"
+                  fill='clear'
                   onClick={() => click(recipe)}
-                  style={{
-                    backgroundImage: `url(${
-                      recipe.image ||
-                      'https://t4.ftcdn.net/jpg/02/33/56/39/360_F_233563961_kE9T55F8EoBCKpKuXnrXTV1bIgQIve7W.jpg'
-                    })`,
-                  }}
-                >
+                  //Will display the image if there is one otherwise will fallback to pan
+                  style={{ backgroundImage: `url(${recipe.image || 'https://t4.ftcdn.net/jpg/02/33/56/39/360_F_233563961_kE9T55F8EoBCKpKuXnrXTV1bIgQIve7W.jpg'})` }}
+                  >
                   <span id="recipe_button_text">{recipe.name}</span>
                 </IonButton>
               </div>
-            ))}
-          </div>
-          )}
+            ))
+        )
+      )}
       </IonContent>
     </IonPage>
-  );
+  )
 };
-
 export default Recipe;
