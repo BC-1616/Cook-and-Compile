72eafd948ac7deae06f1e75b2cb25716c96d58ba
Jacklyn McAtee <jrmcatee1926@students.lcstate.edu>
2025-04-20 15:40:24 -0700

    Changed cypress test to be consistent with new updates

--- capstone/cypress/e2e/mealPlanPopup.cy.ts
+++ capstone/cypress/e2e/mealPlanPopup.cy.ts
@@ -12,18 +12,14 @@ describe("Meal Selector Popup", () => {
 
         cy.visit("/MealPlan"); 
         cy.wait(2000); //wait 2 seconds for popup to load // 1 Second seems too short for all the rendering to take place.
-        cy.contains('Weekly').click();
-        cy.contains('Daily').click();
 
         cy.contains("Add Meal").click();
         cy.wait(2000); //wait 2 seconds for popup to load // 1 Second seems too short for all the rendering to take place.
         cy.get('input[placeholder="Search recipes..."]').type("Test Recipe");
-        cy.get('.recipe-list li').first().click();
-
-        cy.contains("Add Meal").click();
-        cy.wait(2000); //wait 2 seconds for popup to load // 1 Second seems too short for all the rendering to take place.        
+        cy.get('.recipe-list li').first().click();       
         cy.get('input[placeholder="Enter an item..."]').type("Test Item");
-        cy.contains('Close').click();
+        cy.contains("Add Item").click();
+        cy.contains('Save & Close').click();
 
         cy.contains('Test Item').should('exist');
         cy.contains('Test Recipe').should('exist');
